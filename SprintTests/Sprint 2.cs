ff7f9347ca287cc87bf5de0dab8459392e0a0175
Dan1el-Terry <24374442@studentmail.ucol.ac.nz>
2026-09-18 18:12:18 +1200

    Feat: Added Sprint 2 Test Cases

---
+++ SprintTests/Sprint 2.cs
@@ -0,0 +1,73 @@
+﻿using Assign_2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SprintTests
+{
+    public class Sprint_2
+    {
+        //Code for testing Sql injection (one of the requirements for sprint 2)
+        [Fact]
+        public void Login_SQLInjectionAttempt_ShouldFail()
+        {
+            string role;
+
+            bool result = UserDatabase.Login(
+                "' OR 1=1 --",
+                "anything",
+                out role);
+
+            Assert.False(result);
+        }
+        // Test that all users can be retrieved from the database.
+        [Fact]
+        public void GetAllUsers_ReturnsUsers()
+        {
+            var users = UserDatabase.GetAllUsers();
+
+            Assert.NotNull(users);
+            Assert.NotEmpty(users);
+        }
+        //Needs Update, delete, add testing for user
+
+
+        // Test that locations can be retrieved from the database.
+        [Fact]
+        public void GetAllLocations_ReturnsLocations()
+        {
+            var locations = SensorsDatabase.GetAllLocations();
+
+            Assert.NotNull(locations);
+            Assert.NotEmpty(locations);
+        }
+        // Test that sensors can be retrieved from the database.
+        [Fact]
+        public void GetAllSensors_ReturnsSensors()
+        {
+            var sensors = SensorsDatabase.GetAllSensors();
+
+            Assert.NotNull(sensors);
+            Assert.NotEmpty(sensors);
+        }
+        // Test that every sensor has location information.
+        [Fact]
+        public void GetAllSensors_SensorsHaveLocationInformation()
+        {
+            var sensors = SensorsDatabase.GetAllSensors();
+
+            Assert.NotEmpty(sensors);
+
+            foreach (var sensor in sensors)
+            {
+                Assert.False(string.IsNullOrWhiteSpace(sensor.Make));
+                Assert.False(string.IsNullOrWhiteSpace(sensor.Model));
+                Assert.False(string.IsNullOrWhiteSpace(sensor.City));
+                Assert.False(string.IsNullOrWhiteSpace(sensor.Suburb));
+            }
+        }
+    }
+}
